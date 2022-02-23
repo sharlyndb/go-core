aa18a082afd739f47f712861ea4092f76dc92db0
coseman <904497924@qq.com>
2022-02-24 00:47:22 +0800

    新增日志分割器

--- go.mod
+++ go.mod
@@ -13,11 +13,13 @@ require (
 require (
 	github.com/casbin/casbin/v2 v2.41.0
 	github.com/fsnotify/fsnotify v1.5.1
-	github.com/goworkeryyt/go-config v0.0.0-20220223130047-135baa671a73
+	github.com/goworkeryyt/go-config v0.0.0-20220223161623-26798d9dd980
 	github.com/kr/text v0.2.0 // indirect
+	github.com/natefinch/lumberjack v2.0.0+incompatible
 )
 
 require (
+	github.com/BurntSushi/toml v1.0.0 // indirect
 	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
@@ -40,5 +42,6 @@ require (
 	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ zap/zap.go
@@ -0,0 +1,117 @@
+/**
+ * @Time: 2022/2/24 00:32
+ * @Author: yt.yin
+ */
+
+package zap
+
+import (
+	"errors"
+	"fmt"
+	"github.com/goworkeryyt/go-core/global"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+	"time"
+)
+
+// Zap 初始化zap logger
+func Zap() (logger *zap.Logger){
+	// 先判断目录是否存在
+	if ok, _ := logDirectorExists(global.CONFIG.Zap.Director); !ok { // 判断是否有Director文件夹
+		fmt.Printf("create %v directory\n", global.CONFIG.Zap.Director)
+		_ = os.Mkdir(global.CONFIG.Zap.Director, os.ModePerm)
+	}
+	// 调试级别
+	debugLevel := zap.LevelEnablerFunc(func(lev zapcore.Level) bool {
+		return lev == zap.DebugLevel
+	})
+	// 日志级别
+	infoLevel := zap.LevelEnablerFunc(func(lev zapcore.Level) bool {
+		return lev == zap.InfoLevel
+	})
+	// 警告级别
+	warnLevel := zap.LevelEnablerFunc(func(lev zapcore.Level) bool {
+		return lev == zap.WarnLevel
+	})
+	// 错误级别
+	errorLevel := zap.LevelEnablerFunc(func(lev zapcore.Level) bool {
+		return lev >= zap.ErrorLevel
+	})
+	cores := [...]zapcore.Core{
+		getEncoderCore(fmt.Sprintf("./%s/server_debug.log", global.CONFIG.Zap.Director), debugLevel),
+		getEncoderCore(fmt.Sprintf("./%s/server_info.log", global.CONFIG.Zap.Director), infoLevel),
+		getEncoderCore(fmt.Sprintf("./%s/server_warn.log", global.CONFIG.Zap.Director), warnLevel),
+		getEncoderCore(fmt.Sprintf("./%s/server_error.log", global.CONFIG.Zap.Director), errorLevel),
+	}
+	logger = zap.New(zapcore.NewTee(cores[:]...), zap.AddCaller())
+
+	if global.CONFIG.Zap.ShowLine {
+		logger = logger.WithOptions(zap.AddCaller())
+	}
+	return logger
+}
+
+// getEncoderConfig 获取zapcore.EncoderConfig
+func getEncoderConfig() (config zapcore.EncoderConfig) {
+	config = zapcore.EncoderConfig{
+		MessageKey:     "message",
+		LevelKey:       "level",
+		TimeKey:        "time",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		StacktraceKey:  global.CONFIG.Zap.StacktraceKey,
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     CustomTimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.FullCallerEncoder,
+	}
+	switch {
+	case global.CONFIG.Zap.EncodeLevel == "LowercaseLevelEncoder": // 小写编码器(默认)
+		config.EncodeLevel = zapcore.LowercaseLevelEncoder
+	case global.CONFIG.Zap.EncodeLevel == "LowercaseColorLevelEncoder": // 小写编码器带颜色
+		config.EncodeLevel = zapcore.LowercaseColorLevelEncoder
+	case global.CONFIG.Zap.EncodeLevel == "CapitalLevelEncoder": // 大写编码器
+		config.EncodeLevel = zapcore.CapitalLevelEncoder
+	case global.CONFIG.Zap.EncodeLevel == "CapitalColorLevelEncoder": // 大写编码器带颜色
+		config.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	default:
+		config.EncodeLevel = zapcore.LowercaseLevelEncoder
+	}
+	return config
+}
+
+// getEncoder 获取zapcore.Encoder
+func getEncoder() zapcore.Encoder {
+	if global.CONFIG.Zap.Format == "json" {
+		return zapcore.NewJSONEncoder(getEncoderConfig())
+	}
+	return zapcore.NewConsoleEncoder(getEncoderConfig())
+}
+
+// getEncoderCore 获取Encoder的zapcore.Core
+func getEncoderCore(fileName string, level zapcore.LevelEnabler) (core zapcore.Core) {
+	writer := WriteSyncer(fileName) // 使用lumberjack对日志进行分割
+	return zapcore.NewCore(getEncoder(), writer, level)
+}
+
+// CustomTimeEncoder 自定义日志输出时间格式
+func CustomTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format(global.CONFIG.Zap.Prefix + "2006/01/02 - 15:04:05.000"))
+}
+
+// logDirectorExists 判断日志目录是否存在
+func logDirectorExists(path string) (bool, error) {
+	fi, err := os.Stat(path)
+	if err == nil {
+		if fi.IsDir() {
+			return true, nil
+		}
+		return false, errors.New("存在同名文件")
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
