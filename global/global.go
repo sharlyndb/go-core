a4fdd0156fd39cc76e205c35d7e399338470e5b0
coseman <904497924@qq.com>
2022-02-23 23:35:59 +0800

    修改了常用的一些全局变量

--- global/global.go
+++ global/global.go
@@ -9,8 +9,8 @@ import (
 	"github.com/casbin/casbin/v2"
 	"github.com/eclipse/paho.mqtt.golang"
 	"github.com/go-redis/redis/v8"
-	"github.com/goworkeryyt/configs"
-	"github.com/goworkeryyt/go-core/env"
+	"github.com/goworkeryyt/go-config"
+	"github.com/goworkeryyt/go-config/env"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
@@ -32,7 +32,7 @@ var (
 	MQTT *mqtt.Client
 
 	// CONFIGS 全局系统配置
-	CONFIGS configs.Configs
+	CONFIG *goconfig.Config
 
 	// VP 通过 viper 读取的yaml配置文件
 	VP *viper.Viper
