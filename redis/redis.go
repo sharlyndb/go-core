828271ca618ae1da1a9f1eb2b06414c3bd09c30a
coseman <904497924@qq.com>
2022-02-24 14:41:35 +0800

    初始化redis客户端增加了返回值

--- redis/redis.go
+++ redis/redis.go
@@ -10,12 +10,14 @@ import (
 	"github.com/go-redis/redis/v8"
 	"github.com/goworkeryyt/go-core/global"
 	"go.uber.org/zap"
-	"os"
 )
 
 // Redis 初始z化redis客户端
-func Redis() {
+func Redis() *redis.Client{
 	redisCfg := global.CONFIG.Redis
+	if redisCfg.Addr == "" {
+		return nil
+	}
 	db := 0
 	if redisCfg.DB >= 0 &&  redisCfg.DB <= 15{
 		db = redisCfg.DB
@@ -31,11 +33,9 @@ func Redis() {
 	pong, err := client.Ping(context.TODO()).Result()
 	if err != nil {
 		global.LOG.Error("redis connect ping failed, err:", zap.Any("err", err))
-		os.Exit(1)
-		return
+		return nil
 	} else {
 		global.LOG.Info("redis connect ping response:", zap.String("pong",pong))
-		global.REDIS = client
+		return client
 	}
-	return
 }
