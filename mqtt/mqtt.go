523c660f86dde7b9970fe3916889412edac5e881
coseman <904497924@qq.com>
2022-02-25 12:36:43 +0800

    修改了mqtt的配置

--- mqtt/mqtt.go
+++ mqtt/mqtt.go
@@ -16,24 +16,30 @@ import (
 // DefaultMqtt 创建默认的mqtt客户端
 func DefaultMqtt(clientId string)*mqtt.Client {
 	global.LOG.Info("MQTT开始连接......")
-	global.LOG.Info("MQTT连接地址："+global.CONFIG.Mqtt.Url)
-	opts := mqtt.NewClientOptions().AddBroker(global.CONFIG.Mqtt.Url).SetClientID(clientId)
+	config := global.CONFIG.Mqtt
+	global.LOG.Info("MQTT连接地址："+ config.Url)
+	opts := mqtt.NewClientOptions().AddBroker(config.Url).SetClientID(clientId)
 	// 设置mqtt协议版本 4是3.1.1，3是3.1
-	opts.SetProtocolVersion(4)
+	opts.SetProtocolVersion(config.ProtocolVersion)
 	// 客户端掉线服务端不清除session
-	opts.SetCleanSession(true)
+	opts.SetCleanSession(config.CleanSession)
 	// 设置断开后重新连接
-	opts.SetAutoReconnect(true)
-	opts.SetKeepAlive(20 * time.Second)
+	opts.SetAutoReconnect(config.AutoReconnect)
+	// 保活时间
+	opts.SetKeepAlive(time.Duration(config.KeepAlive) * time.Second)
 	// 用户名和密码
-	opts.SetUsername(global.CONFIG.Mqtt.Username)
-	opts.SetMaxReconnectInterval(60 * time.Second)
-	opts.SetPassword(global.CONFIG.Mqtt.Password)
-	opts.SetPingTimeout(10 * time.Second)
-	opts.SetWriteTimeout(10 * time.Second)
-	opts.SetConnectTimeout(10 * time.Second)
+	opts.SetUsername(config.Username)
+	opts.SetPassword(config.Password)
+	// 最大重连间隔
+	opts.SetMaxReconnectInterval(time.Duration(config.MaxReconnectInterval) * time.Second)
+	// 最大ping超时时间
+	opts.SetPingTimeout(time.Duration(config.PingTimeout) * time.Second)
+	// 最大写超时时间
+	opts.SetWriteTimeout(time.Duration(config.WriteTimeout) * time.Second)
+	// 最大连接超时时间
+	opts.SetConnectTimeout(time.Duration(config.ConnectTimeout) * time.Second)
 	// 设置遗言
-	opts.SetWill("last-will", clientId, 1, true)
+	opts.SetWill(config.WillTopic, clientId, 1, false)
 	client := mqtt.NewClient(opts)
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		global.LOG.Error("MQTT连接异常......", zap.Any(" mqtt:", token.Error()))
@@ -44,24 +50,30 @@ func DefaultMqtt(clientId string)*mqtt.Client {
 // Mqtt 连接和订阅
 func Mqtt(clientId string,onConn mqtt.OnConnectHandler,onLost mqtt.ConnectionLostHandler,reConn mqtt.ReconnectHandler) *mqtt.Client {
 	global.LOG.Info("MQTT开始连接......")
-	global.LOG.Info("MQTT连接地址："+global.CONFIG.Mqtt.Url)
-	opts := mqtt.NewClientOptions().AddBroker(global.CONFIG.Mqtt.Url).SetClientID(clientId)
+	config := global.CONFIG.Mqtt
+	global.LOG.Info("MQTT连接地址："+ config.Url)
+	opts := mqtt.NewClientOptions().AddBroker(config.Url).SetClientID(clientId)
 	// 设置mqtt协议版本 4是3.1.1，3是3.1
-	opts.SetProtocolVersion(4)
+	opts.SetProtocolVersion(config.ProtocolVersion)
 	// 客户端掉线服务端不清除session
-	opts.SetCleanSession(true)
+	opts.SetCleanSession(config.CleanSession)
 	// 设置断开后重新连接
-	opts.SetAutoReconnect(true)
-	opts.SetKeepAlive(20 * time.Second)
+	opts.SetAutoReconnect(config.AutoReconnect)
+	// 保活时间
+	opts.SetKeepAlive(time.Duration(config.KeepAlive) * time.Second)
 	// 用户名和密码
-	opts.SetUsername(global.CONFIG.Mqtt.Username)
-	opts.SetMaxReconnectInterval(60 * time.Second)
-	opts.SetPassword(global.CONFIG.Mqtt.Password)
-	opts.SetPingTimeout(10 * time.Second)
-	opts.SetWriteTimeout(10 * time.Second)
-	opts.SetConnectTimeout(10 * time.Second)
+	opts.SetUsername(config.Username)
+	opts.SetPassword(config.Password)
+	// 最大重连间隔
+	opts.SetMaxReconnectInterval(time.Duration(config.MaxReconnectInterval) * time.Second)
+	// 最大ping超时时间
+	opts.SetPingTimeout(time.Duration(config.PingTimeout) * time.Second)
+	// 最大写超时时间
+	opts.SetWriteTimeout(time.Duration(config.WriteTimeout) * time.Second)
+	// 最大连接超时时间
+	opts.SetConnectTimeout(time.Duration(config.ConnectTimeout) * time.Second)
 	// 设置遗言
-	opts.SetWill("last-will", clientId, 1, true)
+	opts.SetWill(config.WillTopic, clientId, 1, false)
 	if onConn != nil {
 		opts.SetOnConnectHandler(onConn)
 	}
